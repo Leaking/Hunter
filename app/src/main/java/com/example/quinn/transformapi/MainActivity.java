7f8f445f2b56ea52f6acdf05cf7b57de2d0e0d9a
Quinn <chenhuazhaoao@gmail.com>
2017-03-06 14:51:50 +0800

    test codes

--- app/src/main/java/com/example/quinn/transformapi/MainActivity.java
+++ app/src/main/java/com/example/quinn/transformapi/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.quinn.transformapi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "<===================");
+        new Test();
+        Log.e(TAG, "===================>");
     }
 }
---
+++ app/src/main/java/com/example/quinn/transformapi/Test.java
@@ -0,0 +1,11 @@
+package com.example.quinn.transformapi;
+
+/**
+ * Created by HuaChao on 2016/7/4.
+ */
+public class Test {
+
+    public Test() {
+
+    }
+}
