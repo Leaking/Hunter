d46b7366d942dbae2ea87c5662593050eee9ee5c
Quinn <chenhuazhaoao@gmail.com>
2017-03-06 14:51:50 +0800

    test codes

--- groovyplug/src/main/groovy/com/quinn/plugin/BlogTransform.groovy
+++ groovyplug/src/main/groovy/com/quinn/plugin/BlogTransform.groovy
@@ -5,12 +5,16 @@ package com.quinn.plugin;
  */
 
 import com.android.build.api.transform.Context
+import com.android.build.api.transform.DirectoryInput
 import com.android.build.api.transform.Format
+import com.android.build.api.transform.JarInput
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformException
 import com.android.build.api.transform.TransformInput
 import com.android.build.api.transform.TransformOutputProvider
+import org.apache.commons.io.FileUtils
+
 
 class BlogTransform extends Transform {
 
@@ -37,24 +41,36 @@ class BlogTransform extends Transform {
     @Override
     void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
 
-        def outDir = outputProvider.getContentLocation("blogdonothing", outputTypes, scopes, Format.DIRECTORY)
+        // Transform的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
+        inputs.each { TransformInput input ->
+            //对类型为“文件夹”的input进行遍历
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                //文件夹里面包含的是我们手写的类以及R.class、BuildConfig.class以及R$XXX.class等
+                MyInject.injectDir(directoryInput.file.absolutePath,"com\\example\\quinn\\transformapi")
+                // 获取output目录
+                def dest = outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes,
+                        Format.DIRECTORY)
 
-        outDir.deleteDir()
-        outDir.mkdirs()
+                // 将input的目录复制到output指定目录
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+            //对类型为jar文件的input进行遍历
+            input.jarInputs.each { JarInput jarInput ->
 
-        inputs.each {
-            it.directoryInputs.each {
-                int pathBitLen = it.file.toString().length()
-                it.file.traverse {
-                    def path = "${it.toString().substring(pathBitLen)}"
-                    if (it.isDirectory()) {
-                        new File(outDir, path).mkdirs()
-                    } else {
-                        if (! path.endsWith("BuildConfig.class")) {
-                            new File(outDir, path).bytes = it.bytes
-                        }
-                    }
+                //jar文件一般是第三方依赖库jar文件
+
+                // 重命名输出文件（同目录copyFile会冲突）
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
                 }
+                //生成输出路径
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                //将输入内容复制到输出
+                FileUtils.copyFile(jarInput.file, dest)
             }
         }
     }
---
+++ groovyplug/src/main/groovy/com/quinn/plugin/MyInject.groovy
@@ -0,0 +1,56 @@
+package com.quinn.plugin
+
+import javassist.ClassPool
+import javassist.CtClass
+import javassist.CtConstructor
+
+public class MyInject {
+
+    private static ClassPool pool = ClassPool.getDefault()
+    private static String injectStr = "System.out.println(\"I Love HuaChao\" ); ";
+
+    public static void injectDir(String path, String packageName) {
+        pool.appendClassPath(path)
+        File dir = new File(path)
+        if (dir.isDirectory()) {
+            dir.eachFileRecurse { File file ->
+
+                String filePath = file.absolutePath
+                //确保当前文件是class文件，并且不是系统自动生成的class文件
+                if (filePath.endsWith(".class")
+                        && !filePath.contains('R$')
+                        && !filePath.contains('R.class')
+                        && !filePath.contains("BuildConfig.class")) {
+                    // 判断当前目录是否是在我们的应用包里面
+                    int index = filePath.indexOf(packageName);
+                    boolean isMyPackage = index != -1;
+                    if (isMyPackage) {
+                        int end = filePath.length() - 6 // .class = 6
+                        String className = filePath.substring(index, end).replace('\\', '.').replace('/', '.')
+                        //开始修改class文件
+                        CtClass c = pool.getCtClass(className)
+
+                        if (c.isFrozen()) {
+                            c.defrost()
+                        }
+
+                        CtConstructor[] cts = c.getDeclaredConstructors()
+                        pool.importPackage("android.util.Log");
+                        if (cts == null || cts.length == 0) {
+                            //手动创建一个构造函数
+                            CtConstructor constructor = new CtConstructor(new CtClass[0], c)
+                            constructor.insertBeforeBody(injectStr)
+                            c.addConstructor(constructor)
+                        } else {
+                            cts[0].insertBeforeBody(injectStr)
+                        }
+                        c.writeFile(path)
+                        c.detach()
+                    }
+                }
+            }
+        }
+    }
+
+
+}
