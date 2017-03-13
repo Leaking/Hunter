295257fb6684b1cb7428a2a7215b80b80ce44ef3
Leaking <chenhuazhaoao@gmail.com>
2017-03-14 01:19:03 +0800

    add annotation

--- app/src/main/java/com/example/quinn/transformapi/Test.java
+++ app/src/main/java/com/example/quinn/transformapi/Test.java
@@ -1,10 +1,13 @@
 package com.example.quinn.transformapi;
 
+import com.example.quinn.Data.Data;
+
 /**
  * Created by HuaChao on 2016/7/4.
  */
 public class Test {
 
+    @Data
     public Test() {
 
     }
