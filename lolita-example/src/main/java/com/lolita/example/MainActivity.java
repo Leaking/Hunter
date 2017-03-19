de6ca31e5f4359ce66311ac8d61f8fdf0a56e8ef
Leaking <chenhuazhaoao@gmail.com>
2017-03-19 22:15:34 +0800

    refactor project structure

---
+++ lolita-example/src/main/java/com/lolita/example/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lolita.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e(TAG, "<===================");
+        new Test();
+        Log.e(TAG, "===================>");
+    }
+}
---
+++ lolita-example/src/main/java/com/lolita/example/Test.java
@@ -0,0 +1,14 @@
+package com.lolita.example;
+
+
+
+/**
+ * Created by HuaChao on 2016/7/4.
+ */
+public class Test {
+
+    @Data
+    public Test() {
+
+    }
+}
