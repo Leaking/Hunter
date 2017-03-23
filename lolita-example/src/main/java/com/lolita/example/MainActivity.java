254da208d6e015317fef6e852963be5850b60562
Leaking <chenhuazhaoao@gmail.com>
2017-03-24 02:03:56 +0800

    插件所使用的依赖，需要时java类型，而非android

--- lolita-example/src/main/java/com/lolita/example/MainActivity.java
+++ lolita-example/src/main/java/com/lolita/example/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e(TAG, "<===================");
         new Test();
+        new Test2();
         Log.e(TAG, "===================>");
     }
 }
