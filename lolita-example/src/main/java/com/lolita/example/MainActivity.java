df39954d2ad82d2e34e7dab1a5a22f82f80e64aa
Leaking <chenhuazhaoao@gmail.com>
2017-03-27 00:41:13 +0800

    update

--- lolita-example/src/main/java/com/lolita/example/MainActivity.java
+++ lolita-example/src/main/java/com/lolita/example/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.lolita.annotations.ArgumentDebug;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
     @Override
+    @ArgumentDebug
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
