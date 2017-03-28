6be53a6d2d7f4d4f547b3a02adaaa60c23c495a7
Quinn <chenhuazhaoao@gmail.com>
2017-03-28 16:47:43 +0800

    upgrade

---
+++ lolita-example/src/main/java/com/lolita/example/BetaTest.java
@@ -0,0 +1,40 @@
+package com.lolita.example;
+
+import android.util.Log;
+
+import com.lolita.annotations.ParameterDebug;
+import com.lolita.annotations.TimingDebug;
+
+/**
+ * Created by Quinn on 24/03/2017.
+ */
+
+@ParameterDebug
+@TimingDebug
+public class BetaTest {
+
+    public static final String TAG = "BetaTest";
+
+    public BetaTest() {
+        this(5);
+        swing(0,9);
+        claw();
+    }
+
+    public BetaTest(int god){
+        Log.i(TAG, "god constructor");
+    }
+
+    private void swing(int a, int b) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void claw() {
+        int a = 5;
+        Log.i(TAG, "claw");
+    }
+}
--- lolita-example/src/main/java/com/lolita/example/MainActivity.java
+++ lolita-example/src/main/java/com/lolita/example/MainActivity.java
@@ -2,23 +2,31 @@ package com.lolita.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.lolita.annotations.ParameterDebug;
 
 
+@ParameterDebug
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
     @Override
-    @ParameterDebug
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG, "<===================");
-        new Test();
-        new Test2();
-        Log.e(TAG, "===================>");
+        new AlphaTest();
+        new BetaTest();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
