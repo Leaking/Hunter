d07d5cc43b0176cd2ca8acc7847fbcc9c82ca411
quinnhzchen <chenhuazhaoao@gmail.com>
2018-09-07 22:17:41 +0800

    java化

--- hunter-plugin-timing/src/main/groovy/com/quinn/hunter/plugin/TimingHunterTransform.java
+++ hunter-plugin-timing/src/main/groovy/com/quinn/hunter/plugin/TimingHunterTransform.java
@@ -1,18 +1,39 @@
 package com.quinn.hunter.plugin;
 
 
+import com.android.build.api.transform.Context;
+import com.android.build.api.transform.TransformException;
+import com.android.build.api.transform.TransformInput;
+import com.android.build.api.transform.TransformOutputProvider;
+import com.android.build.gradle.internal.LoggerWrapper;
 import com.quinn.hunter.plugin.bytecode.TimingWeaver;
 import com.quinn.hunter.transform.HunterTransform;
 
 import org.gradle.api.Project;
+
+import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Created by quinn on 07/09/2018
  */
 public class TimingHunterTransform extends HunterTransform {
 
+    private static final LoggerWrapper logger = LoggerWrapper.getLogger(TimingHunterTransform.class);
+
+    private Project project;
+
     public TimingHunterTransform(Project project) {
         super(project);
+        this.project = project;
+        project.getExtensions().create("hunterExt", HunterExtension.class);
         this.bytecodeWeaver = new TimingWeaver();
+    }
+
+    @Override
+    public void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        HunterExtension hunterExtension = (HunterExtension) project.getExtensions().getByName("hunterExt");
+        logger.info("hunter2 2 ext global " + hunterExtension.global + " on " + hunterExtension.on);
+        super.transform(context, inputs, referencedInputs, outputProvider, isIncremental);
     }
 }
