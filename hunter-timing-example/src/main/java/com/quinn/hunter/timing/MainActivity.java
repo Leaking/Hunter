b97b6a93a785b715dfbf9e0a9421c1f150b6f883
quinnhzchen <chenhuazhaoao@gmail.com>
2018-09-13 22:03:04 +0800

    reformat package

--- hunter-timing-example/src/main/java/com/quinn/hunter/timing/MainActivity.java
+++ hunter-timing-example/src/main/java/com/quinn/hunter/timing/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import com.hunter.library.timing.BlockHandler;
 import com.hunter.library.timing.IBlockManger;
+import com.hunter.library.timing.impl.BlockManagerImpl;
 import com.quinn.hunter.example.black.AlphaTest;
 
 
@@ -13,14 +15,15 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
 
-    IBlockManger iBlockManger;
+    IBlockManger iBlockManger = new BlockManagerImpl();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new AlphaTest();
         new BetaTest();
-
+        BlockHandler.installBlockManager(iBlockManger);
         findViewById(R.id.dump).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
