c7156748f8657c6a52aef87a28be4c64dbd926f8
Leaking <chenhuazhaoao@gmail.com>
2018-10-04 11:59:13 +0800

    整理

--- hunter-debug-library/src/main/java/com/hunter/library/debug/HunterStepTiming.java
+++ hunter-debug-library/src/main/java/com/hunter/library/debug/HunterStepTiming.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by quinn on 17/09/2018
- */
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR}) @Retention(RetentionPolicy.CLASS)
-public @interface HunterDebug {
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR}) @Retention(RetentionPolicy.CLASS)
+public @interface HunterStepTiming {
 }
