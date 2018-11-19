55a784bfd3d0c3ab4bbd2ea8f2a7a95ff6365eec
quinnhzchen <chenhuazhaoao@gmail.com>
2018-11-19 13:51:12 +0800

    bugfix: can't transform the class inherit from IBlockHandler

--- hunter-timing-plugin/src/main/groovy/com/quinn/hunter/plugin/timing/bytecode/TimingClassAdapter.java
+++ hunter-timing-plugin/src/main/groovy/com/quinn/hunter/plugin/timing/bytecode/TimingClassAdapter.java
@@ -3,11 +3,15 @@ package com.quinn.hunter.plugin.timing.bytecode;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+
 import java.io.File;
+import java.util.Arrays;
 
 public final class TimingClassAdapter extends ClassVisitor{
 
     private String className;
+
+    private boolean isHeritedFromBlockHandler = false;
 
     TimingClassAdapter(final ClassVisitor cv) {
         super(Opcodes.ASM5, cv);
@@ -16,6 +20,7 @@ public final class TimingClassAdapter extends ClassVisitor{
     @Override
     public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
         super.visit(version, access, name, signature, superName, interfaces);
+        this.isHeritedFromBlockHandler = Arrays.toString(interfaces).contains("com/hunter/library/timing/IBlockHandler");
         this.className = name;
     }
 
@@ -23,7 +28,11 @@ public final class TimingClassAdapter extends ClassVisitor{
     public MethodVisitor visitMethod(final int access, final String name,
                                      final String desc, final String signature, final String[] exceptions) {
         MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
-        return mv == null ? null : new com.quinn.hunter.plugin.timing.bytecode.TimingMethodAdapter(className + File.separator + name, access, desc, mv);
+        if(isHeritedFromBlockHandler) {
+            return mv;
+        } else {
+            return mv == null ? null : new com.quinn.hunter.plugin.timing.bytecode.TimingMethodAdapter(className + File.separator + name, access, desc, mv);
+        }
     }
 
 
