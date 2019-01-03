82476c232969447a987b9be84bf54f43d6d2b895
quinnhzchen <quinnhzchen@tencent.com>
2019-01-04 01:26:54 +0800

    bugfix: duplicated jars in transforms/dexBuilder

--- hunter-transform/src/main/java/com/quinn/hunter/transform/HunterTransform.java
+++ hunter-transform/src/main/java/com/quinn/hunter/transform/HunterTransform.java
@@ -10,7 +10,6 @@ import com.android.build.api.transform.Transform;
 import com.android.build.api.transform.TransformException;
 import com.android.build.api.transform.TransformInput;
 import com.android.build.api.transform.TransformOutputProvider;
-import com.android.build.gradle.internal.LoggerWrapper;
 import com.android.build.gradle.internal.pipeline.TransformManager;
 import com.android.ide.common.internal.WaitableExecutor;
 import com.quinn.hunter.transform.asm.BaseWeaver;
@@ -18,6 +17,7 @@ import com.quinn.hunter.transform.asm.ClassLoaderHelper;
 
 import org.apache.commons.io.FileUtils;
 import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,7 +32,7 @@ import java.util.Set;
  */
 public class HunterTransform extends Transform {
 
-    private static final LoggerWrapper logger = LoggerWrapper.getLogger(HunterTransform.class);
+    private final Logger logger;
 
     private static final Set<QualifiedContent.Scope> SCOPES = new HashSet<>();
 
@@ -49,6 +49,7 @@ public class HunterTransform extends Transform {
 
     public HunterTransform(Project project){
         this.project = project;
+        this.logger = project.getLogger();
         this.waitableExecutor = WaitableExecutor.useGlobalSharedThreadPool();
     }
 
@@ -86,13 +87,14 @@ public class HunterTransform extends Transform {
         } else if("release".equals(context.getVariantName())) {
             emptyRun = runVariant == RunVariant.DEBUG || runVariant == RunVariant.NEVER;
         }
-        logger.info(getName() + " isIncremental > " + isIncremental + " runVariant > " + runVariant + " emptyRun > " + emptyRun);
+        logger.warn(getName() + " isIncremental > " + isIncremental + " runVariant > " + runVariant + " emptyRun > " + emptyRun);
         long startTime = System.currentTimeMillis();
         if(!isIncremental) {
             outputProvider.deleteAll();
         }
         URLClassLoader urlClassLoader = ClassLoaderHelper.getClassLoader(inputs, referencedInputs, project);
         this.bytecodeWeaver.setClassLoader(urlClassLoader);
+        boolean flagForCleanDexBuilderFolder = false;
         for(TransformInput input : inputs) {
             for(JarInput jarInput : input.getJarInputs()) {
                 Status status = jarInput.getStatus();
@@ -116,6 +118,11 @@ public class HunterTransform extends Transform {
                             break;
                     }
                 } else {
+                    //Forgive me!, Some project will store 3rd-party aar for serveral copies in dexbuilder folder,,unknown issue.
+                    if(!isIncremental && !flagForCleanDexBuilderFolder) {
+                        cleanDexBuilderFolder(dest);
+                        flagForCleanDexBuilderFolder = true;
+                    }
                     transformJar(jarInput.getFile(), dest, status);
                 }
             }
@@ -160,7 +167,7 @@ public class HunterTransform extends Transform {
 
         waitableExecutor.waitForTasksWithQuickFail(true);
         long costTime = System.currentTimeMillis() - startTime;
-        logger.info((getName() + " costed " + costTime + "ms"));
+        logger.warn((getName() + " costed " + costTime + "ms"));
     }
 
     private void transformSingleFile(final File inputFile, final File outputFile, final String srcBaseDir) {
@@ -200,6 +207,29 @@ public class HunterTransform extends Transform {
         });
     }
 
+    private void cleanDexBuilderFolder(File dest) {
+        waitableExecutor.execute(() -> {
+            try {
+                String dexBuilderDir = replaceLastPart(dest.getAbsolutePath(), getName(), "dexBuilder");
+                //intermediates/transforms/dexBuilder/debug
+                File file = new File(dexBuilderDir).getParentFile();
+                project.getLogger().warn("clean dexBuilder folder = " + file.getAbsolutePath());
+                com.android.utils.FileUtils.deleteDirectoryContents(file);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+    }
+
+    private String replaceLastPart(String originString, String replacement, String toreplace) {
+        int start = originString.lastIndexOf(replacement);
+        StringBuilder builder = new StringBuilder();
+        builder.append(originString.substring(0, start));
+        builder.append(toreplace);
+        builder.append(originString.substring(start + replacement.length()));
+        return builder.toString();
+    }
 
     @Override
     public boolean isCacheable() {
